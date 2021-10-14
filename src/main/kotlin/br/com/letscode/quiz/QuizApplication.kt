7cde9d5f5c0ab72f3aa5740575e5070beabd609d
Lucio Oliveira <lucio.oliveira@ifood.com.br>
2021-10-14 19:52:51 -0300

    primeiro commit

---
+++ src/main/kotlin/br/com/letscode/quiz/QuizApplication.kt
@@ -0,0 +1,11 @@
+package br.com.letscode.quiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizApplication>(*args)
+}
